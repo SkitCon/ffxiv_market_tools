339770338e46cba4664d490a6450626432f327cb
SkitCon <ambercconverse@gmail.com>
2024-11-14 03:07:19 -0700

    Added script stub

---
+++ market_main.py
@@ -0,0 +1,12 @@
+'''
+File: market_main.py
+Author: Amber Converse and Killuescent
+Purpose: Main script for profitability calculations. STUB
+'''
+
+def main():
+    # Stub
+    pass
+
+if __name__ == "__main__":
+    main()
